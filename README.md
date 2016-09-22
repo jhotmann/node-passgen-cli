75a3179e64ed7deaaa8a2225dcf236f204757d70
Jordan Hotmann <jhotmann@gmail.com>
2016-09-22 11:10:27 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# How it works
+Type passgen "some passphrase here" into the terminal or command prompt. PassGen takes your input string,
+appends a unique salt string to it, and then generates a hash. The resulting hash is then
+base91 encoded so that it uses all uppercase and lowercase letters, numbers, and standard
+symbols.  A subset of 15-100 (default is 40 and can be changed in the setup) characters
+is picked from the base91 encoded hash and then placed in your clipboard to be pasted into
+a password field.
+
+Every time you use the same input string, you will get the same result from PassGen. This
+allows you to use an easy to remember passphrases to generate lengthy,
+complicated passwords that no one can reproduce unless they know your passphrase, salt,
+and method of generating passwords.
+
+# Installation
+1. Install NodeJS if you haven't already https://nodejs.org
+2. Type `npm install -g passgen-cli` into your terminal or command window
+3. Type `passgen -s` and hit enter to launch the setup process. If you haven't used passgen before a random salt will have been generated for you. Replace this salt with your own if you've already used passgen elsewhere.
+4. Done. You can now generate passwords.
+
+## Links to Dependencies
+- Base91 https://github.com/mscdex/base91.js
+- Skein hash https://github.com/drostie/sha3-js
+- Minimist https://github.com/substack/minimist
+- Prompt https://github.com/flatiron/prompt
+- copy-paste https://github.com/xavi-/node-copy-paste
+- fs-extra https://github.com/jprichardson/node-fs-extra
---
+++ bin.js
@@ -0,0 +1,83 @@
+#!/usr/bin/env node
+var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs-extra');
+var os = require('os');
+var prompt = require('prompt');
+var thecommand = require('./');
+
+// Read config or create it if not exists
+var configDir;
+if (pathExists(os.homedir() + '/Persistent')) { // Tails user
+  configDir = os.homedir() + '/Persistent/.passgen';
+} else { // Non-Tails user
+  configDir = os.homedir() + '/.passgen';
+}
+fs.ensureDirSync(configDir);
+fs.ensureFileSync(configDir + '/settings.json');
+var config = fs.readJsonSync(configDir + '/settings.json', { throws: false });
+if (config === null) {
+  config = {
+    salt: (Math.random() + 1).toString(36).slice(2) + (Math.random() + 1).toString(36).slice(2),
+    length: 40
+  };
+  fs.writeJson(configDir + '/settings.json', config);
+}
+
+// Do stuff based on arguments
+if (argv.help || argv.h) { // Display help
+  console.log('');
+  console.log('Usage: %s "your passphrase here"', 'passgen');
+  console.log('');
+  console.log(' The resulting password will be placed in your clipboard for pasting');
+  console.log('');
+  console.log('Options:');
+  console.log('');
+  console.log(' -h, --help    Display this usage info');
+  console.log(' -s, --setup   Will guide you through the setup process');
+  process.exit(0);
+} else if (argv.setup || argv.s) { // do setup
+  console.log('');
+  console.log('===============================================================================');
+  console.log('==                               PassGen Setup                               ==');
+  console.log('===============================================================================');
+  console.log('');
+  console.log('Input a new value or press enter to keep the existing value (in parentheses)');
+  console.log('');
+  var schema = {
+    properties: {
+      salt: {
+        description: 'Salt: the string that is appended to all your passphrases before generating the password',
+        type: 'string',
+        default: config.salt,
+        required: true
+      },
+      length: {
+        description: 'Length: how long the generated password will be',
+        type: 'integer',
+        default: config.length,
+        required: true
+      }
+    }
+  };
+  prompt.start();
+  prompt.get(schema, function (err, result) {
+    config = {
+      salt: result.salt,
+      length: result.length
+    };
+    fs.writeJson(configDir + '/settings.json', config);
+    console.log('');
+    console.log('Configuration saved!');
+  });
+} else {
+  thecommand(argv._[0], config);
+}
+
+function pathExists(path) {
+  try {
+    fs.accessSync(path, fs.F_OK);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
